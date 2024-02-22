d7ce82bd98aeca552de21124231839defdf2cefa
PaintCreates <adammoore1180@outlook.com>
2024-02-21 19:23:14 -0700

    added the constructors for media objects

--- back_end/models/media.js
+++ back_end/models/media.js
@@ -13,5 +13,65 @@ module.exports = class media {
     }
 }
 
+module.exports = class author {
+
+    constructor (authorID) {
+        this.authorID = authorID;
+    }
+}
+
+module.exports = class media_author {
+
+    constructor (mediaID, authorID) {
+        this.mediaID = mediaID;
+        this.authorID = authorID;
+    }
+}
+
+module.exports = class speaker {
+
+    constructor (speakerID) {
+        this.speakerID = speakerID;
+    }
+}
+
+module.exports = class media_speaker {
+
+    constructor (mediaID, speakerID) {
+        this.mediaID = mediaID;
+        this.speakerID = speakerID;
+    }
+}
+
+module.exports = class actor {
+
+    constructor (actorID) {
+        this.actorID = actorID;
+    }
+}
+
+module.exports = class media_actor {
+
+    constructor (mediaID, actorID) {
+        this.mediaID = mediaID;
+        this.actorID = actorID;
+    }
+}
+
+module.exports = class subtitleoption {
+
+    constructor (subtitleoptionID) {
+        this.subtitleoptionID = subtitleoptionID;
+    }
+}
+
+module.exports = class media_subtitleoption {
+
+    constructor (mediaID, subtitleoptionID) {
+        this.mediaID = mediaID;
+        this.subtitleoptionID = subtitleoptionID;
+    }
+}
+
 // use this line to import class
 // const media = require('./models/media');
